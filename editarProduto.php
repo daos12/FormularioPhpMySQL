80fde2a69db07249363af4dfdc96d130fcac68e6
Diego Antonio Oliveira Silva <11053@senacminas.edu.br>
2022-09-30 12:02:50 -0300

    Atualização sistema, parte de edição

---
+++ editarProduto.php
@@ -0,0 +1,92 @@
+<?php
+    include 'conexao.php';
+    $id = $_GET['id'];
+?>
+
+<!DOCTYPE html>
+<html lang="pr-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Edição Produtos</title>
+</head>
+
+<body>
+    <div class="container tamanhoContainer" style="margin-top: 40px">
+
+        <form action="inserirProduto.php" method="post"> 
+            <!-- Inserir php  -->
+            <?php
+                //Pegar código no sql select 
+                $sql = "SELECT * FROM `produtos` WHERE idEstoque = $id";
+                $buscar = mysqli_query($conexao,$sql);
+                
+                while($array = mysqli_fetch_array($buscar)){
+                //pegar codigo abaixo no documento listarProdutos.php
+                    $idProduto      = $array['idProduto'];
+                    $numeroProduto  = $array['numeroProduto'];
+                    $nomeProduto    = $array['nomeProduto'];
+                    $qtdProduto     = $array['qtdProduto'];
+                    $catProduto     = $array['catProduto'];
+                    $fornProduto    = $array['fornProduto'];
+                
+                    //Finalizar o } no final do form abaixo
+                    //retirar o placeholder e autocomplete e required dos formularios 
+            ?>
+
+            <h3 style="text-align: center;">Editar o produto</h3><br>
+
+            <div class="form-group">
+                <label>Numero do Produto: </label>
+                <!-- Adicionando para traser as infos do banco como get -->
+                <input type="number" class="form-control" name="numeroProduto" value="<?php echo $numeroProduto?>"> 
+
+            </div>
+            <div class="form-group">
+                <label>Nome do Produto: </label>
+                <input type="text" class="form-control" name="nomeProduto value="<?php echo $nomeProduto?>">
+
+            </div>
+            <div class="form-group">
+                <label>Quantidade: </label>
+                <input type="number" class="form-control" name="qtdProduto value="<?php echo $qtdProduto?>" >
+
+            </div>
+            <div class="form-group">
+
+                <label>Categoria</label>
+                <select class="form-control" name="catProduto value="<?php echo $catProduto?>" >
+
+                    <option>Periféricos</option>
+                    <option>Hardware</option>
+                    <option>Software</option>
+                    <option>Headsets</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label>Fornecedores</label>
+                <select class="form-control" name="fornProduto value="<?php echo $fornProduto?>" >
+
+                    <option>Microsoft </option>
+                    <option>Aplee</option>
+                    <option>Lenovo</option>
+                    <option>Multilaiser</option>
+                </select>
+            </div>
+            <div class="botao">
+
+                <button type="submit" class="btn btn-secondary">Atualizar</button> <!-- Alterar type para submit-->
+            </div>
+            <?php } ?>
+        </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ listarProdutos.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Produtos Cadastrados</title>
+</head>
+
+<body>
+    <div class="container">
+        <h3>Listagem de Produtos</h3>
+        <!-- Pegar segunda tabela do bootstrap -->
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Número</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Estoque</th>
+                    <th scope="col">Categoria</th>
+                    <th scope="col">Fornecedor</th>
+                    <th scope="col">Ação</th> <!-- Colocar depois que a visualização funcionar -->
+                </tr>
+            </thead>
+            
+                    <?php
+                    include 'conexao.php';
+                    $sql = "SELECT * FROM `produtos`";
+                    $busca = mysqli_query($conexao, $sql);
+
+                    while ($array = mysqli_fetch_array($busca)) {
+                        $idProduto      = $array['idProduto'];
+                        $numeroProduto  = $array['numeroProduto'];
+                        $nomeProduto    = $array['nomeProduto'];
+                        $qtdProduto     = $array['qtdProduto'];
+                        $catProduto     = $array['catProduto'];
+                        $fornProduto    = $array['fornProduto'];
+                    
+                    ?>
+                <tr>
+                    <td><?php echo $numeroProduto; ?> </td>
+                    <td><?php echo $nomeProduto; ?> </td>
+                    <td><?php echo $qtdProduto; ?> </td>
+                    <td><?php echo $catProduto; ?> </td>
+                    <td><?php echo $fornProduto; ?> </td>
+                    <td><a class="btn btn-secundary" style="text-decoration: none; color: black ;" 
+                        href="editarProduto.php?id=<?php echo $idProduto?>" role="button">Editar</a> 
+                    </td>   <!-- Colocar depois que a visualização funcionar -->
+                            <!-- Mostrar as mudanças do id na parte de baixo do site -->
+                    
+                </tr>
+
+            <?php } ?>
+        </table>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
